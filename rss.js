2f58b722a0342a202f3789218d2b9a2b18f342ae
Mark <mark@bhalash.com>
2016-10-28 19:50:21 +0100

    Updated file syntax.

--- rss.js
+++ rss.js
@@ -1,12 +1,12 @@
-var rss = require('rss'),
-    http = require('http'),
-    opts = require('./thefutureof.json'),
-    episodes = require('require-dir')('./episodes/'),
-    feed = new rss(opts);
+const rss = require('rss');
+const http = require('http');
+const opts = require('./thefutureof.json');
+const episodes = require('require-dir')('./episodes/');
+const feed = new rss(opts);
 
 Object.keys(episodes).forEach(ep => feed.item(episodes[ep]));
 
-http.createServer(function(req, res) {
+http.createServer((req, res) => {
     res.writeHead(200, { 'Content-Type': 'text/xml' });
     res.end(feed.xml());
 }).listen(1509, '0.0.0.0');
